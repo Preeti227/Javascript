3d474b626526811f52d7bcc7c67c5a4f7ac263dd
Preeti Dey <141465009+Preeti227@users.noreply.github.com>
2026-04-09 14:14:46 +0000

    basics

--- 01/test.js
+++ 01/test.js
@@ -1,1 +1,12 @@
+"use strict";//treat all codes as newer versions
+
 console.log('Preeti')
+ 
+/* var is not used now because of issue in block and functional scope*/
+
+let city="Kolkata"
+let college="University of Engineering & Management"
+const name="Preeti"
+
+console.table([city,college,name])
+console.log(typeof(null))//type:object 
