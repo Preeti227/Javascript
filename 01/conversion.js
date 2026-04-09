37fdab16f94b7114b55496f8fcc1767505facff4
Preeti Dey <141465009+Preeti227@users.noreply.github.com>
2026-04-09 18:12:52 +0000

    Comparsion of data types

--- 01/conversion.js
+++ 01/conversion.js
@@ -12,3 +12,16 @@ console.log(typeof(booleanIsLoggedIn))
 
 //"" => false
 // 0 =>false 1=>true
+
+let str1= "Hello"
+let str2="world"
+
+let str3=str1+ str2
+console.log(str3)
+
+console.log("1"+2+2)//122
+console.log(1+2+"2")//32
+
+//=== Strict check (checks value as well as the datatype)
+console.log("2"==2)
+console.log("2"===2)
