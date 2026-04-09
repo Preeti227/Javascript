e24ce4566faa5e00401c182c496a6a0e0869a62e
Preeti Dey <141465009+Preeti227@users.noreply.github.com>
2026-04-09 14:26:31 +0000

    conversion1

---
+++ 01/conversion.js
@@ -0,0 +1,14 @@
+let score="5a5"
+console.log(typeof(score))
+
+let scoreInNum=Number(score)
+console.log(typeof(scoreInNum))//number
+console.log(scoreInNum)//NaN - Not a Number
+
+let isLoggedIn=0
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+console.log(typeof(booleanIsLoggedIn))
+
+//"" => false
+// 0 =>false 1=>true
