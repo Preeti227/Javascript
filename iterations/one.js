add08bdd0c8df7855f3245b7da86bec4d2e4b54f
Preeti Dey <141465009+Preeti227@users.noreply.github.com>
2026-08-07 15:30:53 +0000

    mini dom projects

---
+++ iterations/one.js
@@ -0,0 +1,15 @@
+let myArray=["apple","bat","cat"]
+console.log(myArray.length)
+for(let index=0;index<myArray.length;index++){
+    console.log(myArray[index])
+}
+
+for(let index=0;index<10;index++){
+    console.log(`${index}`)
+}
+
+
+const score=11
+do{
+    console.log(`Score is ${score}`)
+}while(score<=10)
